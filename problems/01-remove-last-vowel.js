47c154bee80176e61f238052b5890becbb9cb1f2
MehwishHanif <mehwish.hanif90@gmail.com>
2024-09-24 09:12:24 -0700

    update

--- problems/01-remove-last-vowel.js
+++ problems/01-remove-last-vowel.js
@@ -5,11 +5,26 @@ Vowels are the letters "a", "e", "i", "o", "u".
 */
 
 // Your code here 
+function removeLastVowel(word){
+    let wordArr = word.split('');
+    let lastIndex=null;
+    let vowels='aeiou';
+    for(let i=0; i<wordArr.length; i++){
+        if(vowels.includes(wordArr[i]))
+            lastIndex=i;
+    }
+    if(lastIndex){
+        wordArr.splice(lastIndex,1);
+        return wordArr.join('');
+    }
+    else
+        return word;
+}
 
-// console.log(removeLastVowel('bootcamp')); // 'bootcmp'
-// console.log(removeLastVowel('better')); // 'bettr'
-// console.log(removeLastVowel('graph')); // 'grph'
-// console.log(removeLastVowel('thy')); // 'thy'
+console.log(removeLastVowel('bootcamp')); // 'bootcmp'
+console.log(removeLastVowel('better')); // 'bettr'
+console.log(removeLastVowel('graph')); // 'grph'
+console.log(removeLastVowel('thy')); // 'thy'
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = removeLastVowel;
